9e5a620084af41846232215243a539a55b0585ea
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-11-30 16:20:46 -0500

    3

--- main.c
+++ main.c
@@ -10,7 +10,16 @@ int main() {
     scanf("%d", &y);
     printf("z: ");
     scanf("%d", &z);
-  
+    int arreglo[x][y][z];
+    
+    // esta parte es para inicializar las matrices en cero
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                arreglo[i][j][k] = 0;
+            }
+        }
+    }
 
     return 0;
 }
