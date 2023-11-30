d348b5c157689d3194a471b32754b85f3ea5c82b
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-11-30 16:19:42 -0500

    2

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-
-
+    int x, y, z;
+    
+    printf("Ingrese las dimensiones del arreglo tridimensional:\n");
+    printf("x: ");
+    scanf("%d", &x);
+    printf("y: ");
+    scanf("%d", &y);
+    printf("z: ");
+    scanf("%d", &z);
+  
 
     return 0;
 }
